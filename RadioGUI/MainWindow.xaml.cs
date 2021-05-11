841611b25185e1cef97de7d009b9c5912402b433
Bongani <Bongtheman@outlook.com>
2021-05-11 11:25:18 +0100

    Initial Commit

---
+++ RadioGUI/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RadioApp;
+
+namespace RadioGUI
+{
+    public partial class MainWindow : Window
+    {
+        Radio radio;
+        public static ManageChannels channelManager = new ManageChannels();
+        public static PlaylistPlayback RadioPlayer = new PlaylistPlayback();
+        
+   
+        public MainWindow()
+        {
+            InitializeComponent();
+            TopFrame.Content = new TopFramePage();
+            MainFrame.Content = RadioPlayer;
+        }
+
+        
+
+        
+    }
+}
